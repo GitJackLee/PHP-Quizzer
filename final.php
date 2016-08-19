595ee195370e1d175a26b132716e4e846d051108
Jack Anderson <andersonjackwork@gmail.com>
2016-08-19 22:40:09 +0900

    Added other necessary files, and additional UI.

---
+++ final.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Quizzer</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+
+    <header>
+      <div class="container">
+          <h1>PHP Quizzer</h1>
+      </div>
+    </header>
+
+    <main>
+      <div class="container">
+        <h2>You're done!</h2>
+        <p>Congratulations! You have completed the test!</p>
+        <p>Final Score: 5</p>
+        <a href="question.php?n=1" class="start">Start Again</a>
+      </div>
+    </main>
+
+    <footer>
+      <div class="container">
+        Copyright &copy; 2015, PHP Quizzer
+      </div>
+    </footer>
+
+  </body>
+</html>
